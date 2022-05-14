458d01094e4ace6ece5ede115995fbc1634f7201
Ali Asad Quasim <aasad2530@gmail.com>
2022-05-14 17:20:56 +0530

    Linked List implementation

---
+++ linkedlist.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+};
+int main()
+{
+    struct Node* head;
+    head->data = 10;
+    head->next = NULL;
+    
+    return 0;
+}
